3494a2bb17ca0d6bc42e110beca11ffbd3d80626
anibal21 <anibalrodriguez1990@gmail.com>
2019-02-24 19:49:19 -0300

    inst docker

--- init.sh
+++ init.sh
@@ -3,9 +3,11 @@
 #We connect to the instance
 #ssh -i /Users/anibalsci/Documents/aws_keys/acid.pem ubuntu@18.188.193.19
 
+#Doker installation
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
-#Docker
-sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+curl -fsSL https://test.docker.com -o test-docker.sh
+sh test-docker.sh
+
+sh install.sh
